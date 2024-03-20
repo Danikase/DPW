bbf2110819da20cd30c3df4ceb03b6c471477453
Christopher <figueroaromero@gmail.com>
2024-03-19 18:21:10 -0600

    avance python

---
+++ DLP/Semana4/Guia4/Ejercicio1.py
@@ -0,0 +1,4 @@
+print("Calcula volumen de una esfera")
+Rad = int(input("type radio here: "))
+Vol = (4/3) * (3.1416) * pow(Rad,3)
+print("es igual a " + str(Vol))
---
+++ DLP/Semana4/Guia4/Ejercicio2.py
@@ -0,0 +1,4 @@
+print("Calcular Area")
+Rad = int(input("Ingrese el radio: "))
+a = (3.1416) * pow(Rad,2)
+print("tu respuesta es: " + str(a))
