5ab5ef97fccca3e12f513cfd97106a20e7854d26
Danikase <titofigueroa97@gmail.com>
2024-03-21 00:33:36 -0600

    avance ejercicio 5 dlp

--- DLP/Semana4/Guia4/Ejercicio5.py
+++ DLP/Semana4/Guia4/Ejercicio5.py
@@ -1,14 +1,17 @@
 print("Calculadora de la hipotenusa de un triángulo rectángulo")
 
-# import math se usa para hacer uso de funciones matemáticas como sqrt 
+# import math se usa para hacer uso de funciones matemáticas como 'sqrt' 
 import math
 
 # Estas seran las dos variables que usaremos para recolectar los datos del usuario
 cateto1  = float(input("Ingrese el valor del primer cateto: "))
 cateto2  = float(input("Ingrese el valor del segundo cateto: "))
 
+# La función math.sqrt() nos permite utilizar raíz cuadrada, la cual utilizamos para el calculo de la hipotenusa
+# Otra manera en la que se pudo haber hecho este ejercicio es en lugar de usar 'pow()' solo se puede usar doble asterisco
+# hipotenusa = math.sqrt(cateto1**2 + cateto2**2)
 hipotenusa = math.sqrt(pow(cateto1,2)+pow(cateto2,2))
-#math.sqrt(pow(cateto1,2)pow(cateto2,2))
-#math.sqrt((cateto1**2)+(cateto2**2))   
-
+ 
+# Mostramos el resultado al usuario 
+# Para mostrar el resultado esta vez no fue necesario convertir la variable en 'str()' porque python lo hizo automaticamente
 print ("La longitud de la hipotenusa es: ", hipotenusa)
