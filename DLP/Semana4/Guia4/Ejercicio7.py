a1197432691f4d4e52bc5d2d63e135851fd326bb
Danikase <titofigueroa97@gmail.com>
2024-03-23 19:16:36 -0600

    avance

--- DLP/Semana4/Guia4/Ejercicio7.py
+++ DLP/Semana4/Guia4/Ejercicio7.py
@@ -1,11 +1,2 @@
 print("tiempo de cocción")
 
-# 
-tiempo_de_coccion = 45
-peso = float(input("peso del pollo en kg: "))
-
-if peso <= 0:
-    print("El peso del pollo debe ser mayor a cero")
-else:
-    if peso == 1 == tiempo_de_coccion:
-        print(f"El tiempo de cocción es de {tiempo_de_coccion}")
